805c202523442fa6dc0c9b0e574f2ae1b551cc20
Jo Hoar <joanne.hoar@robertson.brightspace.com>
2026-02-04 13:15:18 -0700

    Create HomePage and ProductsPage components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import './HomePage.css'
+
+function HomePage(){
+
+    return(
+        <div>
+            <h2>Welcome to Every Day Market</h2>
+            <p>Your one-stop shop for all your daily needs!</p>
+        </div>
+    );
+}
+export default HomePage
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,9 @@
+import './ProductsPage.css'
+
+function ProductsPage(){
+
+    return(
+        <p>Products page works!</p>
+    );
+}
+export default ProductsPage
